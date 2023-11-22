13b776659729f035192442fa1645c267b83f3f32
the-world-at-large <artem-abramov-2002@mail.ru>
2023-11-22 10:00:17 +0200

    Review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -57,13 +57,14 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', 5432)
+        'NAME': os.getenv('POSTGRES_DB', 'default_database_name'),
+        'USER': os.getenv('POSTGRES_USER', 'default_username'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'default_password'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', 5432),
     }
 }
+
 
 
 # Password validation
