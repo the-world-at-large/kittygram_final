5fa4e11a774228d36a916f709d231798df8266fa
the-world-at-large <artem-abramov-2002@mail.ru>
2023-11-27 20:56:50 +0200

    Test

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -97,10 +97,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'collected_static'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
